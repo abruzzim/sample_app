a27a2d724288816455600503b427ea83b01ad6aa
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-16 13:46:33 -0500

    Updated default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_option = { :host => 'heroku_url' }
 end
