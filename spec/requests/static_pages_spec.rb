ce553fe38e25942301ee785ce703b64a2f9e2795
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-16 15:32:44 -0500

    Completed Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Sample App" }
   describe "Home page" do
-    it "has the <title> Home" do
+    it "has the <title> Sample App | Home" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
     it "has the content 'Sample App'" do
       visit '/static_pages/home'
@@ -12,9 +13,9 @@ describe "StaticPages" do
     end
   end
   describe "Help page" do
-    it "has the <title> Help" do
+    it "has the <title> Sample App | Help" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
     it "has the content 'Help'" do
       visit '/static_pages/help'
@@ -22,13 +23,23 @@ describe "StaticPages" do
     end
   end
   describe "About page" do
-    it "has the <title> About Us" do
+    it "has the <title> Sample App | About Us" do
       visit '/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
     it "has the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+  describe "Contact page" do
+    it "has the <title> Sample App | Contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+    it "has the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
