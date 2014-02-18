5ab6def179fcafe00512bf6068b830fd7414d5e2
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-18 17:51:35 -0500

    Refactored RSpec to Use Shared Examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,36 +4,45 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_title(full_title(page_title)) }
+    it { should have_css('h1', text: heading) }
+  end
+
   describe "Home page" do
 
-    before(:each) { visit root_path }
-    it { should have_title(full_title('')) }
+    before { visit root_path }
+    let(:page_title) { '' }
+    let(:heading)    { 'Sample App' }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
-    it { should have_css('h1', text: 'Sample App') }
 
   end
 
   describe "Help page" do
 
     before(:each) { visit help_path }
-    it { should have_title(full_title('Help')) }
-    it { should have_css('h1', text: 'Help') }
+    let(:page_title) { 'Help' }
+    let(:heading)    { 'Help' }
+    it_should_behave_like "all static pages"
 
   end
 
   describe "About page" do
 
     before { visit about_path }
-    it { should have_title(full_title('About')) }
-    it { should have_css('h1', text: 'About') }
+    let(:page_title) { 'About' }
+    let(:heading)    { 'About' }
+    it_should_behave_like "all static pages"
 
   end
 
   describe "Contact page" do
 
     before { visit contact_path }
-    it { should have_title(full_title('Contact')) }
-    it { should have_css('h1', text: 'Contact') }
+    let(:page_title) { 'Contact' }
+    let(:heading)    { 'Contact' }
+    it_should_behave_like "all static pages"
 
   end
 
