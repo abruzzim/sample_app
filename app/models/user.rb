76f98f751282523d524ec3f48f914866db738ad0
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-19 12:06:18 -0500

    User Model Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase }
+  ##before_save { self.email = email.downcase }
+  # Alternative syntax
+  before_save { email.downcase! }
 
   validates :name,  presence: true, 
                     length:   { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  ##VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # Disallow double dots in email addresses.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence:   true, 
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 6 }
-  
+
   # Add Rails 4 secure password method.
   # https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
   # http://api.rubyonrails.org/v4.0.0/classes/ActiveModel/Validations/HelperMethods.html#method-i-validates_confirmation_of
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -90,6 +90,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  # Test the "before_save" callback.
+  describe "; email address with mixed case" do
+    let(:mixed_case_email) { "uSeR@SaMpLe.CoM" }
+    it "saves the email address in all lowercase" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "; when Password is blank" do
     before do
       @user.password              = " "
