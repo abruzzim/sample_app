33d0186b92540856eff8fdb1254f86f8c09bd92c
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-18 19:48:16 -0500

    Added HTML Link Tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,25 +5,54 @@ describe "StaticPages" do
   subject { page }
 
   shared_examples_for "all static pages" do
+
     it { should have_title(full_title(page_title)) }
+
     it { should have_css('h1', text: heading) }
+
+    it "has the correct header layout links" do
+      click_link "Home"
+      expect(page).to have_css('h1', text: 'Sample App')
+      click_link "Help"
+      expect(page).to have_css('h1', text: 'Help')
+      click_link "Sign In"
+      expect(page).to have_css('h1', text: 'Sign In')
+    end
+
+    it "has the correct footer layout links" do
+      click_link "About"
+      expect(page).to have_css('h1', text: 'About')
+      click_link "Contact"
+      expect(page).to have_css('h1', text: 'Contact')
+    end
+
   end
 
   describe "Home page" do
 
-    before { visit root_path }
+    before(:each) { visit root_path }
+
     let(:page_title) { '' }
     let(:heading)    { 'Sample App' }
+
     it_should_behave_like "all static pages"
+
     it { should_not have_title('| Home') }
+
+    it "has a link to Register" do
+      click_link "Register"
+      expect(page).to have_css('h1', text: 'Sign up')
+    end
 
   end
 
   describe "Help page" do
 
     before(:each) { visit help_path }
+
     let(:page_title) { 'Help' }
     let(:heading)    { 'Help' }
+
     it_should_behave_like "all static pages"
 
   end
@@ -31,8 +60,10 @@ describe "StaticPages" do
   describe "About page" do
 
     before { visit about_path }
+
     let(:page_title) { 'About' }
     let(:heading)    { 'About' }
+
     it_should_behave_like "all static pages"
 
   end
@@ -40,8 +71,10 @@ describe "StaticPages" do
   describe "Contact page" do
 
     before { visit contact_path }
+
     let(:page_title) { 'Contact' }
     let(:heading)    { 'Contact' }
+    
     it_should_behave_like "all static pages"
 
   end
