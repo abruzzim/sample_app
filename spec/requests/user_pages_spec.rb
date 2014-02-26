0b89822d8a009865327a24c7f034d4fef25c32fb
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-02-25 21:23:35 -0500

    Added Strong Parameters and Signup Error Handling

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,29 @@ describe "UserPages" do
   describe "new user (signup) page" do
     before { visit signup_path }
 
+    let(:submit) { "Create Account" }
+
     it { should have_css('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign up'))}
+
+    describe "register with invalid information" do
+      it "does not create a new user" do
+        expect{ click_button submit }.to_not change(User, :count)
+      end
+    end
+
+    describe "register with valid information" do
+      before do
+        fill_in "Name",         with: "John Olberding"
+        fill_in "Email",        with: "j.olberding@example.com"
+        fill_in "Password",     with: "Secret"
+        fill_in "Confirmation", with: "Secret"
+      end
+      it "creates a new user" do
+        expect{ click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
   end
 
   describe "show user (profile) page" do
